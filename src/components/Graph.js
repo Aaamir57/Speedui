7cca2c2ec5422df9c21c51d870a2986f07cd70e8
Amir <amirkhurshid57@gmail.com>
2023-12-14 00:17:01 +0500

    first commit

---
+++ src/components/Graph.js
@@ -0,0 +1,53 @@
+/* App.js */
+import React from 'react';
+import CanvasJSReact from '@canvasjs/react-charts';
+
+const CanvasJS = CanvasJSReact.CanvasJS;
+const CanvasJSChart = CanvasJSReact.CanvasJSChart;
+
+const App = () => {
+  const chartContainerStyle = {
+    width: '95%', // Set the width as needed
+    height: '100px' // Set the height as needed
+  };
+
+  const options = {
+    animationEnabled: true,
+    exportEnabled: true,
+    theme: "light2",
+    title: {
+      text: ""
+    },
+    axisY: {
+      includeZero: true
+    },
+    data: [{
+      type: "column",
+      indexLabelFontColor: "#5A5757",
+      indexLabelPlacement: "outside",
+      dataPoints: [
+        { x: 10, y: 71 },
+        { x: 20, y: 55 },
+        { x: 30, y: 50 },
+        { x: 40, y: 65 },
+        { x: 50, y: 71 },
+        { x: 60, y: 68 },
+        { x: 70, y: 38 },
+        { x: 80, y: 92, indexLabel: "Highest" },
+        { x: 90, y: 54 },
+        { x: 100, y: 60 },
+        { x: 110, y: 21 },
+        { x: 120, y: 49 },
+        { x: 130, y: 36 }
+      ]
+    }]
+  };
+
+  return (
+    <div style={chartContainerStyle}>
+      <CanvasJSChart options={options} />
+    </div>
+  );
+};
+
+export default App;

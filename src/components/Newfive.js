79d3952c1d9bcc499dfb10b2525cbc386c2a1244
Amir <amirkhurshid57@gmail.com>
2023-12-18 22:25:51 +0500

    second page

---
+++ src/components/Newfive.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+export default function Newfive() {
+    return (
+        <div className='newfivepro'>
+            <div className='newfivemain'>
+                <div className='newfivemainleft'>
+                    <h1 className='linefive'>No-Questions-Asked</h1>
+                        <h1  className='linefive'>30-Day
+                        <div className='bgnewfive1'>
+                            <h2>Refund Guarantee</h2>
+                        </div> 
+                        </h1>
+                    <h2>Our policy is to serve customers the way we would want to be treated. No questions asked, we provide a
+                        30-day money-back guarantee on all of our products. If you are not satisfied, just request a refund within 30 days.</h2>
+                    <div className='bgnewfive'>
+                        <h2>ASK OUR EXPERT</h2>
+                    </div>
+
+                </div>
+                <div className='newfivemainright'>
+                    <img alt=' ' src="./media/100.png" />
+                </div>
+            </div>
+        </div>
+    )
+}
---
+++ src/components/Newfour.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+export default function Newfour() {
+    return (
+        <div>
+            <div className='newfourmain'>
+                <div className='newfourmainleft'>
+                    <h1>The Best Quality
+                        Hosting Features</h1>
+                    <div className='newfourmainleft1'>
+                        <img alt=' ' src="./media/103.svg" />
+                        <h2><span>Deploy in 30 seconds:</span> Easily and quickly deploy your software or application in
+                            just 30 seconds. This saves your time and effort, and lets you get to work faster.</h2>
+                    </div>
+                    <div className='newfourmainleft1'>
+                        <img alt=' ' src="./media/103.svg" />
+                        <h2><span>Deploy in 30 seconds:</span> Easily and quickly deploy your software or application in
+                            just 30 seconds. This saves your time and effort, and lets you get to work faster.</h2>
+                    </div>
+                    <div className='newfourmainleft1'>
+                        <img alt=' ' src="./media/103.svg" />
+                        <h2><span>Deploy in 30 seconds:</span> Easily and quickly deploy your software or application in
+                            just 30 seconds. This saves your time and effort, and lets you get to work faster.</h2>
+                    </div>
+                    <h3>Explore More Features:</h3>
+                </div>
+               
+                <div className='newfourmainright'>
+                <img alt=' ' src="./media/101.svg" />
+                </div>
+            </div>
+        </div>
+    )
+}
